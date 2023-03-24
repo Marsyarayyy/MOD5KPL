0a3f3111b5285f7cb596632f3b2b9549e29a0ea4
PC16-IFLAB <>
2023-03-24 09:12:08 +0700

    menambahkan class SimpleDataBase

--- JURNAL5_KPL_1302213003/Program.cs
+++ JURNAL5_KPL_1302213003/Program.cs
@@ -1,2 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+public class SimpleDataBase<T>
+{
+    private List<T> storedData;
+    private List<DateTime> inputDates = new List<DateTime>();
+    public SimpleDataBase()
+    {
+        storedData = new List<T>();
+    }
+    public void AddNewData(T data)
+    {
+        storedData.Add(data);
+        inputDates.Add(DateTime.Now);
+    }
+    public void PrintAllData()
+    {
+        for (int i = 0; i < storedData.Count; i++)
+        {
+            Console.WriteLine("Data " + (i + 1) +  " berisi: " + storedData[i] + ", yang disimpan pada waktu" + inputDates[i]);
+        }
+    }
+}
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        SimpleDataBase<float> database = new SimpleDataBase<float>();
+        database.AddNewData(13);
+        database.AddNewData(02);
+        database.AddNewData(21);
+        database.PrintAllData();
+    }
+}
